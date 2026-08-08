66bbc42c1d01de78ce92a88d7958b627ccfa7e66
janhaviphale1 <janhaviphale1@gmail.com>
2026-08-08 22:43:02 +0530

    Add inner and anonymous class demonstration

---
+++ Assignment6/InnerAnonymousDemo.java
@@ -0,0 +1,16 @@
+class Outer {
+    private String message = "Hello from Outer class!";
+
+    class Inner {
+        void display() {
+            System.out.println("Message: " +message);
+        }
+    }
+}
+public class InnerAnonymousDemo {
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        Outer.Inner inner = outer.new Inner();
+        inner.display();
+    }
+}
