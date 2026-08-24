ddc942df353cfe709ccbcfa1ef23b21cb51d0862
janhaviphale1 <janhaviphale1@gmail.com>
2026-08-24 10:38:45 +0530

    Added final method example

---
+++ Assignment9/FinalMethod.java
@@ -0,0 +1,14 @@
+class Parent {
+    final void display() {
+        System.out.println("Final method in parent class");
+    }
+};
+class Child extends Parent {
+    //void display();
+};
+public class FinalMethod{
+    public static void main(String[] args) {
+        Child c = new Child();
+        c.display();
+    }
+};
