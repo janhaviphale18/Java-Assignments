ebf111d08c769cc5d058faeb5079ca285b4129a6
janhaviphale1 <janhaviphale1@gmail.com>
2026-08-24 10:38:37 +0530

    Added final variable example

---
+++ Assignment9/FinalExample.java
@@ -0,0 +1,9 @@
+class FinalExample {
+    final int MAX = 100;
+    void display() {
+        System.out.println("MAX Value: " +MAX);
+    }
+}
+public static void main(String[]  args){
+    new Child;
+}
